ee486c5ea047d31e5ca08adfc678cb791bcc9e3c
Lipno Hideaway <info@lipno20.cz>
2026-03-16 21:07:28 +0100

    Fix config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/lipno-hideaway/',,
+  base: '/lipno-hideaway/',
   build: { outDir: 'dist', sourcemap: false },
 });
